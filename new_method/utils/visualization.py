12c8fd714b4f42152fb462e580c7d52ea2d566ce
sushlokshah <sushlokshahkop3@gmail.com>
2022-12-23 12:21:12 +0530

    final visualization function

--- new_method/utils/visualization.py
+++ new_method/utils/visualization.py
@@ -20,13 +20,18 @@ import torch.utils as torch_utils
 
 def visualize(posterior, gt, prior = None, path = "output.png"):
     post_seq = []
+    prior_seq = []
+    gt_seq = []
+    keys = posterior.keys()
     for i in range(len(posterior)):
-        post_seq.append(posterior[i][0])
-    post_seq = torch_utils.make_grid(posterior[0], nrow=1, padding=0, normalize=False, range=None, scale_each=False, pad_value=0)
-    gt_seq = torch_utils.make_grid(gt[0], nrow=1, padding=0, normalize=False, range=None, scale_each=False, pad_value=0)
+        post_seq.append(posterior[i][keys[0]])
+        prior_seq.append(prior[i][keys[0]])
+        gt_seq.append(gt[i][keys[0]])
+    post_seq = torch_utils.make_grid(post_seq, nrow=1, padding=0, normalize=False, range=None, scale_each=False, pad_value=0)
+    gt_seq = torch_utils.make_grid(gt_seq, nrow=1, padding=0, normalize=False, range=None, scale_each=False, pad_value=0)
     
     if prior!=None:
-        prior_seq = torch_utils.make_grid(prior[0], nrow=1, padding=0, normalize=False, range=None, scale_each=False, pad_value=0)
+        prior_seq = torch_utils.make_grid(prior_seq, nrow=1, padding=0, normalize=False, range=None, scale_each=False, pad_value=0)
         output = torch_utils.make_grid([gt_seq,post_seq,prior_seq], nrow=3, padding=0, normalize=False, range=None, scale_each=False, pad_value=0)
 
     else:
