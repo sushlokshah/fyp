dd09dd29f3867552b346de43d03af36e05ebf26c
Sushant Jha <[sushanthkumar78@gmail.com]>
2022-12-25 14:48:56 +0530

    .

--- new_method/models/organize.py
+++ new_method/models/organize.py
@@ -10,13 +10,11 @@ import torch
 import torch.nn as nn
 
 class Organize_encodings_unet(nn.Module):
-    def __init__(self, in_channels, out_channels, bilinear=True):
+    def __init__(self, in_channels, out_channels):
         super(Organize_encodings_unet, self).__init__()
-        self.bilinear = bilinear
         self.corr_encoder = Corr_Encoder()
         self.encoder = encoder()
-        self.positional_encoder = Positional_encoding(256)
-        self.unet = UNet(in_channels, out_channels, bilinear)
+        self.positional_encoder = Positional_encoding(self.args.model["positional"]['output_channels'])
+        self.unet = UNet(in_channels, out_channels)
 
-    def forward(self, x, last_time_stamp, current_time_stamp, max_time_stamp, batch_size):
-        
+    def forward(self, x):
