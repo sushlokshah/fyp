1ad3d2c3bbf33984101e7755ef3c2b9398790051
sushlokshah <sushlokshahkop3@gmail.com>
2023-01-05 09:46:53 +0530

    added print statement in kernel estimator

--- new_method/models/kernel_estimator.py
+++ new_method/models/kernel_estimator.py
@@ -22,15 +22,18 @@ class Kernel_estimation(nn.Module):
     
     def forward(self, input, kernel):
         N, C, H, W = input.shape
+        print("kernel_estimation_input",input.shape)
         # print(input.shape, kernel.shape)
         input = self.pad(input)
         unfolded_input= input.unfold(2,self.kernel_size,1).unfold(3,self.kernel_size,1)
         
         N, C_kernel, H, W = kernel.shape
+        print("kernel:",kernel.shape)
         kernel = kernel.reshape(N,H,W,-1)
         kernel = kernel.reshape(N,H,W,self.kernel_size,self.kernel_size, -1)
         kernel = kernel.view(N,-1,H,W,self.kernel_size,self.kernel_size)
         # print(kernel.shape, unfolded_input.shape)
+        print("kernel_estimation_output",kernel.shape)
         output = kernel*unfolded_input
         output = output.sum(4).sum(4)
         output = self.relu(output)
